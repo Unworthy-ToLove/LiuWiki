1b3d1d55822cd200673b5bbb3fea9d8626e3b004
liu <hlsdzpdw@petalmail.com>
2022-09-30 14:49:20 +0800

    1.1 创建SpringBoot项目

---
+++ src/main/java/cn/ll/LiuWikiApplication.java
@@ -0,0 +1,13 @@
+package cn.ll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ll/LiuWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
