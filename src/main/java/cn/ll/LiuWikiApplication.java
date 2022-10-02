605a9463257b92769b8fcdb25c2ccb484826ff1b
liu <hlsdzpdw@petalmail.com>
2022-10-02 23:19:12 +0800

    1.3 修改启动文案

--- src/main/java/cn/ll/LiuWikiApplication.java
+++ src/main/java/cn/ll/LiuWikiApplication.java
@@ -1,13 +1,23 @@
 package cn.ll;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class LiuWikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(LiuWikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(LiuWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(LiuWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+
+
     }
 
 }
