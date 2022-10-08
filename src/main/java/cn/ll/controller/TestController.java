77f86622c50f318119c108a11dc9aece0c1995fc
liu <hlsdzpdw@petalmail.com>
2022-10-08 10:53:59 +0800

    1.8 SpringBoot配置文件,添加自定义配置项 `SpringBoot`会自动识别下面这组配置文件： application.properties/yml config/application.properties/yml 配置文件优先级： yml文件>properties config目录下配置优先于其父目录。

--- src/main/java/cn/ll/controller/TestController.java
+++ src/main/java/cn/ll/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.ll.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,10 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
+    //  @RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World!" + testHello;
     }
 
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
 
 }
