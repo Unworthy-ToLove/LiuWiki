fc071e49109aff42026ee075740570db262e19cb
liu <hlsdzpdw@petalmail.com>
2022-10-12 12:06:48 +0800

    2.6 增加ebook表列表查询

---
+++ src/main/java/cn/ll/service/EbookService.java
@@ -0,0 +1,22 @@
+package cn.ll.service;
+
+import cn.ll.domain.Ebook;
+import cn.ll.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author liuli
+ */
+@Service
+public class EbookService {
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+
+}
