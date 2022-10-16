2867ff7406e6f5870c480b6be52152652f40f168
liu <hlsdzpdw@petalmail.com>
2022-10-17 01:09:08 +0800

    4.3 使用ref实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,24 +46,44 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+        {{ebooks}}
+      </pre>
     </a-layout-content>
   </a-layout>
 
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import axios from "axios";
 
 export default defineComponent({
   name: 'Home',
 
+
   setup(){
-    console.log("setup")
-    axios.get("http://localhost:8080/ebook/list?name=spring").then(function (response) {
-      console.log(response);
-    })
+    console.log("setup");
+    const ebooks = ref();
+
+
+
+
+    onMounted(function (){
+      console.log("onMounted");
+      axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+
+        console.log(response);
+
+
+      });
+    });
+    return{
+      ebooks,
+    }
+
   }
 });
 </script>
