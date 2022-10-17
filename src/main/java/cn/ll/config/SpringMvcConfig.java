5f64a1c97fbbe3119a6475af6cc73fa9c7ff2f2b
liu <hlsdzpdw@petalmail.com>
2022-10-17 21:56:28 +0800

    4.16 配置拦截器，打印接口耗时

---
+++ src/main/java/cn/ll/config/SpringMvcConfig.java
@@ -0,0 +1,23 @@
+package cn.ll.config;
+
+import cn.ll.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author liuli
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
